84ed8312dbe277e4102754dbb243f3739e3683bc
LeeWingPiu <99600383+LEE-Wing-Piu-Jimmy@users.noreply.github.com>
2023-01-24 14:12:05 +0800

    added description of the SequenceBreakerAI
    as the title

--- SquenceBreaker.py
+++ SquenceBreaker.py
@@ -1,3 +1,22 @@
-# Break a sequence of gestures into seperate gestures for classifying
-# Rule of breaking is to evaluate variance of the data
-# mutithreaded design could be used
+""" 
+By getting a sequence of gesture data, break a sequence of gestures into seperate gestures for classifying
+Rule of breaking is to evaluate variance of the data inside a window
+mutithreaded design could be used in the future
+
+Arguments
+---------
+sequence: a sequence of mutiple gestures
+
+Returns
+-------
+a list of gestures for clasification
+"""
+class SequenceBreaker:
+
+    WINDOW_SIZE = 10 #??
+    LIST_OF_GESTURES = []
+
+    def slide():
+        """ 
+        
+        """
